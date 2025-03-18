ff5b3fa7ecaf088563c548d4930fbcd751f6c8a0
clifflee365 <clifflee1022@gmail.com>
2025-03-19 00:12:39 +0800

    chore: Setup Styling

--- app/index.tsx
+++ app/index.tsx
@@ -3,13 +3,9 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className="flex-1 justify-center items-center"
     >
-      <Text>Welcom to RN world.</Text>
+      <Text className="text-5xl text-dark-200 font-bold">Welcom to RN.</Text>
     </View>
   );
 }
